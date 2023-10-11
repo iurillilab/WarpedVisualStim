7111cb861bb0a1b4ed524ee33fc7e3d59af3808d
paolahydra <patella.paola@gmail.com>
2023-10-11 12:27:33 +0200

    Update retinotopic_mapping_singleStims.py
    testing

--- WarpedVisualStim/examples/retinotopic_mapping_singleStims.py
+++ WarpedVisualStim/examples/retinotopic_mapping_singleStims.py
@@ -55,7 +55,7 @@ ds_color_weights = (1., 1., 1.)
 
 # ============================ generic stimulus parameters ========================
 pregap_dur = 0.
-postgap_dur = 0.
+postgap_dur = 3.
 background = 0.
 coordinate = 'degree'
 # =================================================================================
